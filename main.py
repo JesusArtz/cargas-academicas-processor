c73e692ad29d17ff9abf72f9ee3dd0c17c86eae5
Chuy <ivanmndozaolvera@gmail.com>
2025-10-08 11:17:05 -0600

    Feat: Added verification to files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import base64
 from flask import jsonify, request
 from __init__ import APP
 import process
+import verify
 
 
 @APP.route("/procesar_pdf", methods=["POST"])
@@ -19,7 +20,21 @@ def procesar():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-    
+@APP.route("/verificar_pdf", methods=["POST"])
+def verificar():
+    data = request.json
+    pdf_b64 = data.get("pdf_b64")
+
+    if not pdf_b64:
+        return jsonify({"error": "No se recibió PDF en base64"}), 400
+
+    try:
+        pdf_bytes = base64.b64decode(pdf_b64)
+        resultado = verify.verificar_pdf(pdf_bytes)
+        return jsonify(resultado)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     APP.run(debug=True, host="0.0.0.0")
